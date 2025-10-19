998b8650157c4ed57e10baf671dc999991560ca6
Hama Toshiya <63279483+toshi-hm@users.noreply.github.com>
2025-10-19 15:58:07 +0900

    Add playback API support and tests

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,11 +1,13 @@
 """Service layer packages for the application."""
 
 from .file_service import FileService
+from .playback_service import PlaybackService
 from .training_dispatcher import TrainingDispatcher, training_dispatcher
 from .training_service import TrainingService
 
 __all__ = [
   "FileService",
+  "PlaybackService",
   "TrainingService",
   "TrainingDispatcher",
   "training_dispatcher",
